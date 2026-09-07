5cb55f87cf408188b48a204fb1d759d5740bae65
Cunhe <161113619+Cunhe@users.noreply.github.com>
2026-09-08 01:28:26 +0800

    feat: restore full package.json, prefs hook, toast and waveform

---
+++ src/components/svg.img.tsx
@@ -0,0 +1,9 @@
+export const AkariHideWall: React.FC = () => (
+    <img className="akari-hide-wall" alt="SEAWIND" src={"./svg/seawind-mark.svg"} crossOrigin="anonymous" />
+);
+export const AkariNotFound: React.FC = () => (
+    <img className="akari-not-found" alt="not found" src={"./svg/seawind-mark.svg"} crossOrigin="anonymous" />
+);
+export const AkariOdangoLoading: React.FC = () => (
+    <img className="akari-odango-loading start-loading" alt="loading" src={"./svg/seawind-mark.svg"} crossOrigin="anonymous" />
+);
